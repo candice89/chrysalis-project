774d0f99437124d687991c3f6674de745ee832d4
Tang Candice Lin <tanglincandice@gmail.com>
2019-12-08 22:54:38 +0900

    commit

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-var ID_TO_SRC = {
+let ID_TO_BUTTON = {
+  "doS0G8m9ESqqlf87": {
+    "text":"Before Japan",
+    "class":"U1J4egV6Qo4DhTR6",
+  },
+  "Vot3VP4N7SN7vH8q":{
+    "text":"Studied in Japan",
+    "class":"CZ0XZqHR04wT5hFz",
+  },
+  "W5M5p26ezqe7DKMj":{
+    "text":"Work in Japan",
+    "class":"U1J4egV6Qo4DhTR6",
+  },
+  "C6cVFRFZZGkiau1P":{
+    "class":"CZ0XZqHR04wT5hFz",
+    "text":"What next?",
+  },
+};
+let ID_TO_SRC = {
   "doS0G8m9ESqqlf87":"res/before_japan.jpg",
   "Vot3VP4N7SN7vH8q":"res/studied_in_japan.jpg",
   "W5M5p26ezqe7DKMj":"res/work_in_japan.jpg",
   "C6cVFRFZZGkiau1P":"",
 };
+let states = {
+  "doS0G8m9ESqqlf87":true,
+  "Vot3VP4N7SN7vH8q":true,
+  "W5M5p26ezqe7DKMj":true,
+  "C6cVFRFZZGkiau1P":true,
+};
 function makeshift(id){
-  var element = document.getElementById(id);
+  let element = document.getElementById(id);
   element.innerHTML = "";
-  if(ID_TO_SRC[id].length > 0) {
-    var myimg = document.createElement("img");
-    myimg.src=ID_TO_SRC[id];
-    myimg.style.height = "auto";
-    myimg.style.width = "100%";
+
+  let myimg = null;
+  if(states[id]) {
+    if(ID_TO_SRC[id].length > 0) {
+      myimg = document.createElement("img");
+      myimg.src=ID_TO_SRC[id];
+      myimg.style.height = "auto";
+      myimg.style.width = "100%";
+    } else {
+      myimg = document.createElement("div");
+      myimg.setAttribute("class","typewriter");
+      let h1 = document.createElement("h1");
+      h1.innerHTML="I wish I'd know...";
+      myimg.appendChild(h1);
+    }
   } else {
-    var myimg = document.createElement("div");
-    myimg.setAttribute("class","typewriter");
-    var h1 = document.createElement("h1");
-    h1.innerHTML="I wish I'd know...";
-    myimg.appendChild(h1);
+    myimg = document.createElement("button");
+    myimg.setAttribute("class",ID_TO_BUTTON[id]["class"]);
+    myimg.innerHTML = ID_TO_BUTTON[id].text;
   }
+  myimg.onclick = function(){makeshift(id)}
+  states[id] = !states[id];
   element.appendChild(myimg);
 }
